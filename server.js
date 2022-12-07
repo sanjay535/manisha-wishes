8ec83c79d63ed79ab37b975831f4893079d2e664
Sanjay Gautam <sanjaykumargautam535@gmail.com>
2022-12-07 18:57:47 +0530

    edit using netlify

--- server.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-
-const app=express();
-const PORT=process.env.PORT || 4545;
-app.use(express.static(__dirname+'/public'));
-
-app.listen(PORT, ()=>{
-    console.log(`Server started on ${PORT}`)
-})
